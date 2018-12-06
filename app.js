4756ff986b673c488a83897d2dfa323a3b91c7ce
Shahnawaz Khan <shahnawaz-khan@hotmail.com>
2018-12-06 10:56:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-app.get('/', (req,res) => res.send('Hello Azure DevOps, I am nodejs-express javascript minimal app.'));
+app.get('/', (req,res) => res.send('Hello Azure DevOps, I am nodejs-express javascript minimal app deloyed on azure web app.'));
 
 app.listen(port, () => console.log(`Sample app listening on port ${port}.`));
