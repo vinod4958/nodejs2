461b5d79bb238a53b96849d01e72ecce3e704a86
Shahnawaz Khan <shahnawaz-khan@hotmail.com>
2019-01-04 11:23:02 +0530

    Edit app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3001;
 const HOST = '0.0.0.0';
 
-app.get('/', (req,res) => res.send('Hello Azure DevOps, I am nodejs-express javascript minimal app deloyed on azure web app.'));
+app.get('/', (req,res) => res.send('Hello Azure DevOps, I am nodejs-express javascript minimal app.'));
 
 var port = process.env.PORT||PORT;
 app.listen(port, () => console.log(`Running on http://${HOST}:${port}`));
