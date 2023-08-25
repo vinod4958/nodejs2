9223c526db54a3ab3def6c21aba78bfebeba9bfe
vinod4958 <68851068+vinod4958@users.noreply.github.com>
2023-08-25 08:24:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3001;
 const HOST = '0.0.0.0';
 
-app.get('/', (req,res) => res.send('Hello Azure DevOps, I am nodejs-express javascript minimal app.'));
+app.get('/', (req,res) => res.send('Hello Azure DevOps, I am nodejs-express javascript minimal app.we are learning azure devops.'));
 
 var port = process.env.PORT||PORT;
 app.listen(port, () => console.log(`Running on http://${HOST}:${port}`));
